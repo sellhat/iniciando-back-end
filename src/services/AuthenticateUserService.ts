4f7995566e26a36cbe392c36759904604c1282b6
Thalles <programadoram@hotmail.com>
2020-07-22 18:53:09 -0300

    Finalizando o backend
    autenticação, autorização, avatar, etc.

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,8 +1,9 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
-import User from '../models/User';
 import authConfig from '../config/auth';
+import AppError from '../errors/AppError';
+import User from '../models/User';
 
 interface Request {
   email: string;
@@ -21,13 +22,13 @@ class AuthenticateUserService {
     const user = await usersRepository.findOne({ where: { email } });
 
     if (!user) {
-      throw new Error('Incorrect email/passoword combination.');
+      throw new AppError('Incorrect email/passoword combination.', 401);
     }
 
     const passwordMatched = await compare(password, user.password);
 
     if (!passwordMatched) {
-      throw new Error('Incorrect email/passoword combination.');
+      throw new AppError('Incorrect email/passoword combination.', 401);
     }
 
     const { secret, expiresIn } = authConfig.jwt;
